0c2378743efc55e55afeb9eabaedc8c9dee15b46
mathieu renaud <mathieurenaud@mathieus-MacBook-Air.local>
2026-03-22 15:24:14 -0500

    UI update — button alignment + footer fix

--- Code.js
+++ Code.js
@@ -1012,7 +1012,7 @@ function newTimeEntry() {
     html.newRow = 7;
   }
 
-  const htmlOutput = html.evaluate().setWidth(690).setHeight(340);
+  const htmlOutput = html.evaluate().setWidth(690).setHeight(300);
   SpreadsheetApp.getUi().showModelessDialog(htmlOutput, "Nouvelle entrée de temps");
 }
 
